73c01f907e04af4f21467a63eda53fa6a63a5036
idlelimey <idlelimey@outlook.com>
2025-07-03 13:04:52 +0100

    Change primary TW color

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
         variants: {
             variant: {
                 default:
-                    'bg-primary text-primary-foreground shadow-xs hover:bg-primary/90',
+                    'bg-primary text-foreground shadow-xs hover:bg-primary/90',
                 destructive:
                     'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
                 outline:
@@ -20,7 +20,7 @@ const buttonVariants = cva(
                 ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
                 link: 'text-primary underline-offset-4 hover:underline',
                 hotelmap:
-                    'bg-primary text-white hover:bg-primary/90 text-md !px-6 uppercase font-bold tracking-widest',
+                    'bg-primary hover:bg-primary-dark text-white text-md !px-6 uppercase font-bold tracking-widest',
             },
             size: {
                 default: 'h-9 px-4 py-2 has-[>svg]:px-3',
